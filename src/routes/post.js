b270675df5a45093f071d0dcaba6ded5757cf743
sayash chaudhary <sayashchaudhary@gmail.com>
2020-06-28 02:53:10 +0530

    route for my posts

--- src/routes/post.js
+++ src/routes/post.js
@@ -21,4 +21,23 @@ router.post('/createpost', authenticated, (req, res) => {
     })
 });
 
+// router.get('/allposts', authenticated, (req, res) => {
+//     Post.find().populate('postedBy', '_id name')
+//         .then((posts) => {
+//             res.json({ posts })
+//         }).catch((err) => {
+//         console.log(err)
+//     })
+// })
+
+router.get('/myposts', authenticated, (req, res) => {
+    Post.find({ postedBy: req.user._id })
+        .populate('postedBy', '_id name')
+        .then((posts) => {
+            res.json({ posts })
+        }).catch((err) => {
+        console.log(err)
+    })
+})
+
 module.exports = router
